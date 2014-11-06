eb7898ae32f34a01e0fb0774cbeddd9c617ff26f
Vorlog <vorlog@yandex.ru>
2014-11-06 09:57:35 +0300

    Create option.py

---
+++ option.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__author__ = 'vorlog'
+
+
+
+# Mysql connection parametrs
+db = 'asteriskcdrdb' # Database name ( 'asteriskcdrdb' is default)
+server = 'localhost' # Server address ( 'localhost' is default )
+username = ''        # Root username
+password = ''        # Root password
+port = '3306'        # Default port 3306
+charset = 'utf-8'    # Charset for Database table
+
+#E-mail connection parametrs
+email_from = 'your_mail@domain.com'
+recipients = ['your_mail@domain.com']
+smtp_server = 'smtp.domain.com'
+email_port = 25                        #Default SMTP port: 25
+username = 'your_mail@domain.com'
+passwd = 'your_password'
